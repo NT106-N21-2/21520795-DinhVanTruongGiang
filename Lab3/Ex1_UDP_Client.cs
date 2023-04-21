39a09ed6c817e7859e81ea5a170cfeb7460286c7
ConGiansBay <truonggiangnsl123@gmail.com>
2023-04-21 09:35:00 +0700

    Update MainForm, Ex1 Forms

---
+++ Lab3/Ex1_UDP_Client.cs
@@ -0,0 +1,10 @@
+namespace Lab3
+{
+    public partial class Ex1_UDP_Client : Form
+    {
+        public Ex1_UDP_Client()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Lab3/Lab3_MainForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Lab3
+{
+    partial class Lab3_MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(68, 53);
+            button1.Name = "button1";
+            button1.Size = new Size(157, 75);
+            button1.TabIndex = 0;
+            button1.Text = "Exercise 1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.AutoSize = true;
+            button2.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(292, 53);
+            button2.Name = "button2";
+            button2.Size = new Size(157, 75);
+            button2.TabIndex = 0;
+            button2.Text = "Exercise 2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.AutoSize = true;
+            button3.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(518, 53);
+            button3.Name = "button3";
+            button3.Size = new Size(157, 75);
+            button3.TabIndex = 0;
+            button3.Text = "Exercise 3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.AutoSize = true;
+            button4.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(68, 190);
+            button4.Name = "button4";
+            button4.Size = new Size(157, 75);
+            button4.TabIndex = 0;
+            button4.Text = "Exercise 4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.AutoSize = true;
+            button5.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.Location = new Point(292, 190);
+            button5.Name = "button5";
+            button5.Size = new Size(157, 75);
+            button5.TabIndex = 0;
+            button5.Text = "Exercise 5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.AutoSize = true;
+            button6.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            button6.Location = new Point(518, 190);
+            button6.Name = "button6";
+            button6.Size = new Size(157, 75);
+            button6.TabIndex = 0;
+            button6.Text = "EXIT";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // Lab3_MainForm
+            // 
+            AutoScaleDimensions = new SizeF(11F, 22F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(718, 342);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Lab3_MainForm";
+            Text = "Lab3_MainForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+    }
+}
